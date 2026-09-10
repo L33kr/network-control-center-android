94fd1d3b5b7c3841a3cb64672874dc4792dda24b
L33kr <greeed2312@gmail.com>
2026-09-10 23:51:58 +1000

    Mark aggressive native alpha4 build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.github.l33kr.networkcontrolcenter"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.5.1-dev"
+        versionCode = 7
+        versionName = "0.5.2-alpha4"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a")
