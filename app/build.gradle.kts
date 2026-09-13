9d2929f57145f644fd14ae842b2cfa22c9aee90f
L33kr <greeed2312@gmail.com>
2026-09-13 16:43:33 +1000

    Bump rebuild to 0.5.2 beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.github.l33kr.networkcontrolcenter"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.5.1-beta"
+        versionCode = 7
+        versionName = "0.5.2-beta"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a")
